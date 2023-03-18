b70ea9a46fcde12e2e3e13c927ec6a24264234c5
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-03-18 23:41:34 +0100

    BLL upload

---
+++ ATM.BLL/Interface/IAdminOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using ATM.BLL.Models;
+using ATM.DAL.Model;
+using Microsoft.VisualBasic;
+
+namespace ATM.BLL.Interface
+{
+    public interface IAdminOperations
+    {
+        Task<(bool successful, string msg)> UpdateCustomerRecordsAsync(Admin admin, UpdateCustomer User);
+
+        Task<(bool successful,string msg)> CreateNewCustomerRecordAsync(Admin admin, Customer customer);
+
+        Task<(bool successful, string msg)> DeleteCustomerRecordsAsync(Admin admin, string CustomerCardNo);
+
+        Task<IEnumerable<Workflow>> GetAdminWorkFlowAsync(Admin admin);
+
+        Task<IEnumerable<Complains>> GetComplainsAsync();
+    }
+}
+
